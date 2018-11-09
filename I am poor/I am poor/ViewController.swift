295ca48c8a94aceb53cec912e60fd61e05854ce6
smhemel <sm_hemel@yahoo.com>
2018-11-09 23:33:16 +0600

    Dicee

---
+++ I am poor/I am poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by S M HEMEL on 9/11/18.
+//  Copyright © 2018 S M HEMEL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
