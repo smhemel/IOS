85a0d78839e45cc1c45722dd60c3466b697ba7b5
smhemel <sm_hemel@yahoo.com>
2018-11-14 00:44:59 +0600

    Dice

--- Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
+        
     }
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        diceImageViewOne.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageViewTwo.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+    
 
 }
 
