8ff975f2e80a6846b44ef8f54ed54391d8d18794
smhemel <sm_hemel@yahoo.com>
2018-11-20 12:33:32 +0600

    update

--- Quizzler/Quizzler/ViewController.swift
+++ Quizzler/Quizzler/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var questionNumber : Int = 0
+    var score: Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -20,32 +21,51 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        nextQuestion()
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        checkAnswer(sender.tag == 1 ? true : false)
+        questionNumber += 1
+        nextQuestion()
     }
     
     
     func updateUI() {
-      
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNumber + 1) / 13"
+        progressBar.frame.size.width = (view.frame.size.width/13) * CGFloat(questionNumber + 1)
     }
     
 
     func nextQuestion() {
-        
+        if(questionNumber>12) {
+            let alert = UIAlertController(title: "Awesome", message: "You've finished all the questions, do you want to start over?", preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { (UIAlertAction) in
+                    self.startOver()
+            })
+            alert.addAction(restartAction)
+            present(alert, animated: true, completion: nil)
+        }
+        else {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+            updateUI()
+        }
     }
     
     
-    func checkAnswer() {
+    func checkAnswer(_ ans: Bool) {
+        allQuestions.list[questionNumber].answer == ans ? ProgressHUD.showSuccess("Correct") : ProgressHUD.showError("Wrong!")
+        score += (allQuestions.list[questionNumber].answer == ans) ? 1 : 0
         
     }
     
     
     func startOver() {
-       
+        score = 0
+        questionNumber = 0
+        nextQuestion()
     }
     
 
