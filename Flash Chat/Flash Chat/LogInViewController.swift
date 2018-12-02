8d85a6c11fe3b24d9ec4ac5a1a41e2d0a5cd9d96
smhemel <sm_hemel@yahoo.com>
2018-12-02 15:31:23 +0600

    Flash Chat Complete

--- Flash Chat/Flash Chat/LogInViewController.swift
+++ Flash Chat/Flash Chat/LogInViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class LogInViewController: UIViewController {
 
@@ -26,13 +27,14 @@ class LogInViewController: UIViewController {
    
     @IBAction func logInPressed(_ sender: AnyObject) {
 
-        
+        SVProgressHUD.show()
         //TODO: Log in the user
         Auth.auth().signIn(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
             (user, error) in
             if error != nil {
-                print(error)
+                print(error!)
             } else {
+                SVProgressHUD.dismiss()
                 self.performSegue(withIdentifier: "goToChat", sender: self)
             }
         }
--- Flash Chat/Flash Chat/RegisterViewController.swift
+++ Flash Chat/Flash Chat/RegisterViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class RegisterViewController: UIViewController {
 
@@ -29,15 +30,15 @@ class RegisterViewController: UIViewController {
   
     @IBAction func registerPressed(_ sender: AnyObject) {
         
-
+        SVProgressHUD.show()
         //TODO: Set up a new user on our Firbase database
         Auth.auth().createUser(withEmail: emailTextfield.text!, password: passwordTextfield.text!) {
             (user, error) in
             if error != nil {
                 print(error!)
             } else {
-                //success
-                print("Registration Successful!")
+                //print("Registration Successful!")
+                SVProgressHUD.dismiss()
                 self.performSegue(withIdentifier: "goToChat", sender: self)
             }
         }
